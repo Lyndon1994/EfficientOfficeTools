5ffc7f59ea3e7f8c89e4d007408124f63686b974
linyi05 <linyi05@baidu.com>
2021-05-25 20:37:35 +0800

    add chrome plug-in

--- Alfred/README.md
+++ Alfred/README.md
@@ -11,3 +11,16 @@
 
     自己写的插件，包含时间戳转换、进制转换、编码转换等。
     Some useful tools, include time conversion, hexadecimal conversion, string codec, and so on.
+    
+    1. 时间戳转换
+    ![时间戳转换](./image/Xnip2021-05-25_20-26-16.jpg)
+    ![时间戳转换](./image/Xnip2021-05-25_20-18-01.jpg)
+    > 注意，该插件本质用的是/usr/local/bin/gdate，如果没有该命令，需要安装，命令：`brew install coreutils`
+
+    2. 进制转换
+    ![进制转换](./image/Xnip2021-05-25_20-23-08.jpg)
+    ![进制转换](./image/Xnip2021-05-25_20-24-21.jpg)
+
+    3. 编码转换
+    ![编码转换](./image/Xnip2021-05-25_20-25-08.jpg)
+    ![编码转换](./image/Xnip2021-05-25_20-25-44.jpg)
---
+++ chrome/百度助手/baidu.js
@@ -0,0 +1,47 @@
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+	if (info.menuItemId == "baidu_search") {
+		window.open('https://www.baidu.com/s?ie=utf-8&wd=' + encodeURIComponent(info.selectionText));
+	}
+});
+
+chrome.runtime.onInstalled.addListener(function () {
+	chrome.contextMenus.create({
+		"title": "使用百度搜索“%s”",
+		"id": "baidu_search",
+		"contexts": ["selection"]
+	});
+});
+
+function parseUrl(url) {
+	let obj = {}
+	let reg = /([^?=&]+)=([^?=&]+)/g
+	url.replace(reg, function () {
+		obj[arguments[1]] = arguments[2]
+	})
+	return obj
+}
+
+chrome.commands.onCommand.addListener(function (command) {
+	if (command === 'toggle-search') {
+		chrome.tabs.getSelected(null, function (tab) {
+			urlObj = parseUrl(tab.url)
+			if (tab.url.search("baidu.com/s") != -1) {
+				query = urlObj['wd'] || urlObj['word'];
+				if (query) {
+					chrome.tabs.update({ url: 'https://www.google.com.hk/search?ie=utf-8&q=' + query });
+					window.close();
+					// window.open('https://www.google.com/search?ie=utf-8&q=' + query);
+				}
+			}
+			if (tab.url.search("google.com.hk/search") != -1) {
+				query = urlObj['q'];
+				if (query) {
+					chrome.tabs.update({ url: 'https://www.baidu.com/s?ie=utf-8&wd=' + query });
+					window.close();
+					// window.open('https://www.baidu.com/s?ie=utf-8&wd=' + query);
+				}
+			}
+		});
+	}
+})
